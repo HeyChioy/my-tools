4d6113f7c875b0e1b127221a48c1a6f15fce42e3
KyuuSeiRyuu <chioy@foxmail.com>
2017-10-13 07:00:55 -0500

    修改进度算法

--- tampermonkey/BiliBiliCoin/BiliBiliCoin.user.js
+++ tampermonkey/BiliBiliCoin/BiliBiliCoin.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         BiliBili 自动投币
 // @namespace    BiliBiliAutoCoin.kyuuseiryuu
-// @version      0.2
+// @version      1.0
 // @description  BiliBili 自动投币
 // @author       KyuuSeiryuu
 // @match        http://www.bilibili.com/video/av*
 // @match        https://www.bilibili.com/video/av*
 // @grant        GM_addStyle
+// @homepage     https://github.com/HeyChioy/my-tools/tree/master/tampermonkey/BiliBiliCoin
 // ==/UserScript==
 
 (function() {
     'use strict';
-    console.log({ GM_addStyle });
     const storage = { id: 0 };
     const giveCoin = () => {
         GM_addStyle(`.coin-wrap.fade-in, .wnd-mask { display: none !important; }`);// 样式覆盖
@@ -20,16 +20,35 @@
             $('.coin-sure.b-btn').click(); // 确认投币
         });
     };
+    const formatHMS = (timeString) => {
+        const arr = timeString.split(':');
+        const len = arr.length;
+        const hms = {
+            s: +arr[len - 1] || 0,
+            m: +arr[len - 2] || 0,
+            h: +arr[len - 3] || 0
+        };
+        hms.m += parseInt(hms.s / 60, 10);
+        hms.s = hms.s % 60;
+        hms.h += parseInt(hms.m / 60, 10);
+        hms.m = hms.m % 60;
+        return hms;
+    };
+    const totalSeconds = (hms) => {
+        return hms.h * 60 * 60 + hms.m * 60 + hms.s;
+    };
     const checkTime = (totalTime, nowTime) => {
-        return totalTime.split(':')[0] === nowTime.split(':')[0];
+        const total = totalSeconds(formatHMS(totalTime));
+        const now = totalSeconds(formatHMS(nowTime));
+        return now / total > 0.3;
     };
     const startCheck = () => {
-        console.info('当视频播放完后将自动投币');
+        console.info('自动投币启动');
         storage.id = setInterval(() => {
             const totalTime = $('.bilibili-player-video-time-total').text();
             const nowTime = $('.bilibili-player-video-time-now').text();
             if (checkTime(totalTime, nowTime)) {
-                console.log('视频已播放完～');
+                console.log('已投币～');
                 clearInterval(storage.id);
                 giveCoin();
             }
